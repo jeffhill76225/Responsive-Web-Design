fb3526df9bcb45957632f65057bb7a7f56acc510
cameronwp <cameron.w.pittman@gmail.com>
2016-03-16 21:41:07 -0700

    updates viewport-quiz

--- quizzes/viewport-quiz.js
+++ quizzes/viewport-quiz.js
@@ -1,7 +1,18 @@
-var tests = [
-  {
-    test: isViewportSet,
-    desc: "The viewport has been set correctly."
+var suite1 = GE.registerSuite({
+  name: "Set Viewport",
+  code: 'viewportsFTW!'
+})
+
+suite1.registerTest({
+  description: "A meta tag describes the viewport.",
+  active_test: function (iWant) {
+    return iWant.onlyOneOf.theseNodes('meta').attribute('name').toEqual('viewport');
   }
-];
-runGradeLoop(tests, "viewportsFTW!");
+})
+
+suite1.registerTest({
+  description: "A meta tag sets the content.",
+  active_test: function (iWant) {
+    return iWant.onlyOneOf.theseNodes('meta').attribute('content').toEqual('width=device-width, initial-scale=1.0');
+  }
+})
